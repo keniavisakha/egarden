ddd44dacd027655a0dd00eebdf77863a8b37fa9f
Kenia Visakha Zerlinda <s4622439@student.uq.edu.au>
2022-04-13 10:13:23 +1000

    Initial commit

---
+++ Assets/Scripts/AnimationTriggers/TestTrigger.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestTrigger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        AnimTestTrigger();
+    }
+
+    void AnimTestTrigger() {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GetComponent<Animator>().Play("test_anim");
+        }
+    }
+}
